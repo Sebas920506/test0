5fb90aacecf691cd74b43928fb3f4e7df42e055d
Sebas920506 <148593210+Sebas920506@users.noreply.github.com>
2023-10-20 19:33:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # Crear un DataFrame simple
 df = pd.DataFrame({
-    'Mensaje': ['¡Hola mundo!']
+    'Mensaje': ['En canadá esto no funcionó ¯\_(ツ)_/¯']
 })
 
 # Mostrar el DataFrame
