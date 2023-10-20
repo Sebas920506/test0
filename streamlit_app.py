1ddc13cf62436358776793d0b4659b61bb6d2aa3
Sebas920506 <148593210+Sebas920506@users.noreply.github.com>
2023-10-20 19:35:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 
 # Crear un DataFrame simple
-df = st.write('En canadá esto no funcionó ¯\_(ツ)_/¯' })
+df = st.write('En canadá esto no funcionó ¯\_(ツ)_/¯' )
 
 # Mostrar el DataFrame
 st.dataframe(df)
